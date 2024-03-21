0796a7c2cd6cebfe9f44ae19c280b35bae8fc43d
Igor Silva <igormiguel37@gmail.com>
2024-03-21 19:42:24 -0300

    Checkpoint finalizado

---
+++ CP1ABDWN/MVCCheckpoint1/Controllers/Controller2.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCCheckpoint1.Controllers
+{
+    public class Controller2 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CP1ABDWN/MVCCheckpoint1/Controllers/Controller3.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCCheckpoint1.Controllers
+{
+    public class Controller3 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
